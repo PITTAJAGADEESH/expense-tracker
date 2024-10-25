dd83f346cfb4e7b9d36968913808fa3ab0b99410
PITTAJAGADEESH <suryajagadeesh192@gmail.com>
2024-10-25 21:40:27 +0530

    changes

--- db.js
+++ db.js
@@ -1,6 +1,5 @@
 const sqlite3 = require("sqlite3").verbose();
 const db = new sqlite3.Database("./expense-tracker.db");
-
 db.serialize(() => {
   db.run(`CREATE TABLE IF NOT EXISTS transactions (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
